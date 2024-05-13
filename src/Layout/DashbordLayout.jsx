50dade71e48671df3a950e38dae520bd671f83b1
FM-Akib <muntasirakib08@gmail.com>
2024-05-14 01:33:31 +0600

    added dashboard Navigation

--- src/Layout/DashbordLayout.jsx
+++ src/Layout/DashbordLayout.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import NavBar from "../components/Shared/NavBar";
 
 const DashbordLayout = () => {
@@ -13,18 +13,18 @@ const DashbordLayout = () => {
     }
 
     return (
-        <>
-         <NavBar></NavBar>
-     <div className="relative flex h-screen min-w-full py-16">
+    <>
+      <NavBar></NavBar>
+       <div className="relative flex h-screen min-w-full py-16">
    
         {/* Navigation */}
         <div className={`bg-red-600 text-white w-full md:w-1/5 z-10 ${isNavOpen ? 'absolute' : 'hidden'} md:block`}>
-          <div className="p-4">
+          <div className="p-6">
             <h1 className="text-xl font-bold">Dashboard</h1>
             <ul className="mt-4">
-              <li className="py-2" onClick={closeNav}>Link 1</li>
-              <li className="py-2" onClick={closeNav}>Link 2</li>
-              <li className="py-2" onClick={closeNav}>Link 3</li>
+             <Link to="/" className=" "> <li className="py-2 transition duration-300 ease-in-out hover:bg-white hover:text-red-600 font-semibold  px-4 rounded-sm " onClick={closeNav}>Link 1 </li></Link>
+              <li className="py-2 transition duration-300 ease-in-out hover:bg-white hover:text-red-600 font-semibold  px-4 rounded-sm " onClick={closeNav}>Link 2</li>
+              <li className="py-2 transition duration-300 ease-in-out hover:bg-white hover:text-red-600 font-semibold  px-4 rounded-sm " onClick={closeNav}>Link 3</li>
               {/* Add more navigation links as needed */}
             </ul>
           </div>
@@ -32,17 +32,23 @@ const DashbordLayout = () => {
   
         {/* Content */}
         <div className="w-full md:w-4/5">
+
           {/* Toggle Button for Mobile */}
-          <button
-            className="md:hidden fixed bottom-4 right-4 z-20 bg-gray-800 text-white rounded-full p-3"
-            onClick={toggleNav}
-          >
+          <button className="md:hidden fixed bottom-4 right-4 z-20 bg-gray-800 text-white rounded-full p-3"
+            onClick={toggleNav}>
             {isNavOpen ? 'Close' : 'Menu'}
           </button>
   
+
+
+
+
           <div className="p-4">
            <Outlet></Outlet>
           </div>
+
+
+          
         </div>
       </div></>
     );
