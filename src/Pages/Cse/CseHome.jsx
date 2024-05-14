595a7d7a0ef579214338d252c9a13bb0bfe3c31a
FM-Akib <muntasirakib08@gmail.com>
2024-05-14 16:02:36 +0600

    contribution page added

--- src/Pages/Cse/CseHome.jsx
+++ src/Pages/Cse/CseHome.jsx
@@ -18,6 +18,37 @@ const CseHome = () => {
             </div>
         </div>
 
+
+
+       
+
+<div className="relative mx-auto mt-5">
+    <div className="rounded-xl p-1" style={{backgroundImage: "linear-gradient(to right bottom, rgb(79, 70, 229) 0%, rgb(165, 56, 164) 50%, rgb(220, 38, 38) 100%)"}}>
+        <div className="rounded-lg bg-white backdrop-blur">
+            <div className="flex w-full flex-wrap items-center justify-between gap-4 px-8 py-10 sm:px-16 lg:flex-nowrap">
+                <div className="lg:max-w-xl">
+                    <h2 className="block w-full pb-2 bg-gradient-to-b from-white to-gray-800 bg-clip-text font-bold text-transparent text-3xl sm:text-4xl">
+                        Contribute in this resource hub 
+                    </h2>
+                    <p className="my-4 bg-transparent font-medium leading-relaxed tracking-wide text-gray-600">
+                        Natural Language Processing (NLP): The SmartAI Assistant understands and interprets natural
+                        language commands, allowing users to communicate with it effortlessly.
+                    </p>
+                </div>
+                <div className="flex flex-wrap items-center justify-center gap-6 ">
+                    <button className="bg-[#9a031e] hover:bg-[#181313] text-white font-semibold button-text flex items-center justify-center whitespace-nowrap rounded-md transition-all duration-300 px-8 py-3 text-xs sm:text-sm tracking-widest">Contribute</button>
+                    <button className="flex items-center justify-center whitespace-nowrap rounded-md border border-zinc-700 bg-zinc-900 text-center text-white backdrop-blur transition-all hover:bg-zinc-800 px-8 py-3 text-xs sm:text-sm">Learn More</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+    
+
         </div>
     );
 };
