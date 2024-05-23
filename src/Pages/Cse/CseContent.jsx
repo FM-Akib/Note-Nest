af617e56e2fce0056b4b1d7f8b123d828f1fd9e5
FM-Akib <muntasirakib08@gmail.com>
2024-05-23 10:35:30 +0600

    head added dashboard

--- src/Pages/Cse/CseContent.jsx
+++ src/Pages/Cse/CseContent.jsx
@@ -7,6 +7,7 @@ import useAxiosPublic from "../../Hooks/useAxiosPublic";
 import { AuthContext } from "../../Providers/AuthProvider";
 import Swal from "sweetalert2";
 import { v4 as uuidv4 } from 'uuid';
+import { FcDoNotInsert } from "react-icons/fc";
 
 
 const CseContent = () => {
@@ -126,7 +127,7 @@ const CseContent = () => {
         </summary>
 
         <div className="mt-4  leading-relaxed text-gray-700">
-          {midPlaylist.map((item, i) => (
+          {midPlaylist?.length? midPlaylist?.map((item, i) => (
             <div key={i} className="flex flex-col md:flex-row  items-center justify-center mb-4 ">
               <div className="flex border-2 p-4 items-center justify-between w-full max-w-5xl bg-gray-200 rounded-xl group space-x-6 bg-opacity-50 shadow-xl hover:rounded-2xl">
                 <img className="block w-full md:w-1/3 h-40 rounded-lg mx-0" alt="art cover" src={item.imgCover} />
@@ -160,7 +161,7 @@ const CseContent = () => {
                           fillRule="evenodd" 
                           clipRule="evenodd" 
                           d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22ZM16 14.0455V11.5488C16 9.40445 16 8.3323 15.4142 7.66615C14.8284 7 13.8856 7 12 7C10.1144 7 9.17157 7 8.58579 7.66615C8 8.3323 8 9.40445 8 11.5488V14.0455C8 15.5937 8 16.3679 8.32627 16.7062C8.48187 16.8675 8.67829 16.9688 8.88752 16.9958C9.32623 17.0522 9.83855 16.5425 10.8632 15.5229C11.3161 15.0722 11.5426 14.8469 11.8046 14.7875C11.9336 14.7583 12.0664 14.7583 12.1954 14.7875C12.4574 14.8469 12.6839 15.0722 13.1368 15.5229C14.1615 16.5425 14.6738 17.0522 15.1125 16.9958C15.3217 16.9688 15.5181 16.8675 15.6737 16.7062C16 16.3679 16 15.5937 16 14.0455Z" 
-                          fill="#9A031E"
+                          fill="#F59E0B"
                         />
                       </svg>
                       </button>
@@ -181,7 +182,7 @@ const CseContent = () => {
                 </div>
               </div>
             </div>
-          ))}
+          )) : <p className="text-lg ml-10 flex items-center"> <FcDoNotInsert className="mr-2" /> <span className="text-red-600">No one contribute yet.</span> </p>}
         </div>
       </details>
        
--- src/Pages/Dashboard/UserHome.jsx
+++ src/Pages/Dashboard/UserHome.jsx
@@ -24,7 +24,7 @@ const UserHome = () => {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
             </div>
             <div className="stat-title">Total Stars</div>
-            <div className="stat-value text-primary">{totalStar}</div>
+            <div className="stat-value text-primary">{totalStar?totalStar:'0' }</div>
             <div className="stat-desc">By Contribution you earned</div>
         </div>
         
@@ -33,7 +33,7 @@ const UserHome = () => {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
             </div>
             <div className="stat-title">Total Contribution</div>
-            <div className="stat-value text-secondary">{myContribution?.length}</div>
+            <div className="stat-value text-secondary">{myContribution?.length?myContribution?.length : '0'}</div>
             <div className="stat-desc">Thank you for your contribution</div>
         </div>
         
