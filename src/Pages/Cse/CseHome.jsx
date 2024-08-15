61eeeb7341935dd815a5e5334ed2d847b19e5322
FM-Akib <muntasirakib08@gmail.com>
2024-08-15 13:58:11 +0600

    added EEE

--- src/Pages/Cse/CseHome.jsx
+++ src/Pages/Cse/CseHome.jsx
@@ -4,6 +4,10 @@ import '../../App.css';
 import { AuthContext } from '../../Providers/AuthProvider';
 import { useContext } from 'react';
 import { Helmet } from 'react-helmet';
+import pattern from '../../assets/pattern3.jpg'
+
+
+
 const CseHome = () => {
     const {user} = useContext(AuthContext)
 
@@ -17,7 +21,7 @@ const CseHome = () => {
             <div className="absolute inset-0 h-36 opacity-90 lg:h-full"
                 style={{ backgroundImage: "url('data:image/svg+xml,%3Csvg xmlns=\"http://www.w3.org/2000/svg\" width=\"100\" height=\"100\" viewBox=\"0 0 100 100\"%3E%3Cg fillRule=\"evenodd\"%3E%3Cg fill=\"%23e0e7ff\" fill-opacity=\"1\"%3E%3Cpath opacity=\".5\" d=\"M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z\"/%3E%3Cpath d=\"M6 5V0H5v5H0v1h5v94h1V6h94V5H6z\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')" }}>
             </div>
-            <div className="relative mx-auto max-w-7xl px-6 pt-16 pb-12 sm:px-12 lg:pt-14">
+            <div className="relative mx-auto max-w-7xl px-6 pt-10 pb-12 sm:px-12 lg:pt-14">
                 <header className="mx-auto max-w-2xl text-center">
                     <h1 className="text-3xl font-extrabold tracking-tight text-slate-900 sm:text-4xl">Computer Science & Engineering</h1>
                     <p className="mt-2 text-lg font-semibold text-gray-400 tiro-bangla-regular">
@@ -30,8 +34,8 @@ const CseHome = () => {
   
 
 <div className="relative mx-auto mt-5">
-    <div className="rounded-xl p-1" style={{backgroundImage: "linear-gradient(to right bottom, rgb(79, 70, 229) 0%, rgb(165, 56, 164) 50%, rgb(220, 38, 38) 100%)"}}>
-        <div className="rounded-lg bg-white backdrop-blur">
+    <div className="rounded-xl p-1 bg-cover bg-center" style={{ backgroundImage: `url(${pattern})` }}>
+        <div className="rounded-lg bg-white/50 ">
             <div className="flex w-full flex-wrap items-center justify-between gap-4 px-8 py-10 sm:px-16 lg:flex-nowrap">
                 <div className="lg:max-w-xl">
                     <h2 className="block w-full pb-2 bg-gradient-to-b from-white to-gray-800 bg-clip-text font-bold text-transparent text-3xl sm:text-4xl">
--- src/Pages/EEE/EEEHome.jsx
+++ src/Pages/EEE/EEEHome.jsx
@@ -5,6 +5,7 @@ import { AuthContext } from '../../Providers/AuthProvider';
 import { useContext } from 'react';
 import { Helmet } from 'react-helmet';
 import pattern from '../../assets/pattern3.jpg'
+import EEE from '../../assets/eee.jpg'
 
 
 const EEEHome = () => {
@@ -17,13 +18,13 @@ const EEEHome = () => {
     </Helmet>
                    {/* Heading */}
         <div className="relative border-t border-gray-200 bg-gray-50 mb-2">
-            <div className="absolute inset-0 h-36 opacity-90 lg:h-full"
-                style={{ backgroundImage: "url('data:image/svg+xml,%3Csvg xmlns=\"http://www.w3.org/2000/svg\" width=\"100\" height=\"100\" viewBox=\"0 0 100 100\"%3E%3Cg fillRule=\"evenodd\"%3E%3Cg fill=\"%23e0e7ff\" fill-opacity=\"1\"%3E%3Cpath opacity=\".5\" d=\"M96 95h4v1h-4v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4h-9v4h-1v-4H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15v-9H0v-1h15V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9V0h1v15h9zm-1 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm9-10v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-10 0v-9h-9v9h9zm-9-10h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9zm10 0h9v-9h-9v9z\"/%3E%3Cpath d=\"M6 5V0H5v5H0v1h5v94h1V6h94V5H6z\"/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')" }}>
+            <div className="absolute inset-0 h-36 opacity-90 lg:h-full bg-cover bg-center"
+                style={{ backgroundImage: `url(${EEE})` }}>
             </div>
-            <div className="relative mx-auto max-w-7xl px-6 pt-16 pb-12 sm:px-12 lg:pt-14">
+            <div className="relative mx-auto max-w-7xl px-6 pt-10 pb-12 sm:px-12 lg:pt-14 bg-white/75">
                 <header className="mx-auto max-w-2xl text-center">
-                    <h1 className="text-3xl font-extrabold tracking-tight text-slate-900 sm:text-4xl">Electrical and Electronics Engineering</h1>
-                    <p className="mt-2 text-lg font-semibold text-gray-400 tiro-bangla-regular">
+                    <h1 className="text-2xl font-extrabold tracking-tight text-slate-900 sm:text-4xl">Electrical and Electronics Engineering</h1>
+                    <p className="mt-2 text-lg font-semibold text-gray-600 tiro-bangla-regular">
                     &quot;আঁরা বেজ্ঞুন সি এস ই ইঞ্জিনিয়ার&quot;
                     </p>
                 </header>
