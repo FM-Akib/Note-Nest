c9d2deb291e3dc9c8a4d608c71839c1618ebb840
FM-Akib <muntasirakib08@gmail.com>
2024-11-01 21:50:53 +0600

    updated top contributors

--- src/Pages/Landing/TopContributor.jsx
+++ src/Pages/Landing/TopContributor.jsx
@@ -25,19 +25,25 @@ const TopContributor = () => {
             <div className="py-4 bg-white border-[1px]  shadow-xl border-slate-300 rounded-xl flex ">
             <Marquee >
                 {
-                    users?.map(auser=>  <div key={auser._id}  className="flex items-center flex-col ml-4 space-y-4 sm:flex-row sm:space-y-0 sm:space-x-6 p-4 border-e-2 ">
+                    users?.map((auser, index)=>  <div key={auser._id}  className="flex items-center flex-col ml-4 space-y-4 sm:flex-row sm:space-y-0 sm:space-x-6 p-4 border-e-2 ">
                     {/* <img className="object-cover w-20 h-20 mt-3 mr-3 rounded-full" src="https://www.fakepersongenerator.com/Face/male/male20171086011485596.jpg" /> */}
-                    <div className="avatar">
+                  
+
+                        
+                    <div className="avatar ">
                         <div className="w-14 rounded-full ring ring-red-600 ring-offset-base-100 ring-offset-2">
                             <img src={auser.image} />
                         </div>
-                        </div>
+                    </div>
+
+
                     <div>
                         <p className="font-display mb-1 text-lg font-semibold text-black" >
                             <a href="#" rel="author">{auser.name}</a>
                         </p>
                         <div className=" prose prose-sm text-gray-400">
                             <p className="font-semibold">Total Stars - {calculateTotalStars(auser.myContribution)}</p>
+                            <p className="font-semibold">Position - <span className="text-red-600 px-1 bg-emerald-100 rounded-lg">{index+1}</span></p>
                         </div>
          
                     </div>
